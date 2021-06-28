a1e183a18cefc573b1eeeb93168ee0f2350430b9
Tom Brander <tom.e.brander@gmail.com>
2021-06-28 18:07:32 +0200

    login,register, newrecipe, getrecipies
    recipe class also in progress

---
+++ recept_skafferiet_app/lib/recipe.dart
@@ -0,0 +1,20 @@
+
+class Recipe{
+  var instructions;
+  var ingridients;
+  var title;
+  var description;
+  var id;
+  var url;
+  var personalRating;
+  var time;
+  
+
+  Recipe(ins, ing, tit, des){
+    this.ingridients = ing;
+    this.instructions = ins;
+    this.title = tit;
+    this.description = des;
+  }
+
+}
