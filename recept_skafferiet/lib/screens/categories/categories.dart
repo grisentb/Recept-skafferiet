f3f5c2b1ab758cb6c217666f3b8efa15ef8a3837
tohsic-7 <tohsic-7@student.ltu.se>
2021-07-30 13:13:48 +0200

    temp fix

--- recept_skafferiet/lib/screens/categories/categories.dart
+++ recept_skafferiet/lib/screens/categories/categories.dart
@@ -24,7 +24,7 @@ class _CategoriesPageState extends State<Categories> {
 
   _getCategories() async {
     await this.dbComm.connectToCollections();
-    List cats = await this.dbComm.getCategories(session['username']);
+    List cats = await this.dbComm.getCategories_old(session['username']);
     setState(() {
       this.categories = cats;
       loaded = true;
